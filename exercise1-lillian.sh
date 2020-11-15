d52f448e3ffa4b7452c60beb4713506aa8704ac1
jliu389 <71359373+jliu389@users.noreply.github.com>
2020-11-15 08:45:15 -0500

    Delete exercise1-lillian.sh

--- exercise1-lillian.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#This script takes as input one argument, a directory, and outputs the number of different permission types in this directory.
-
-ls -l $1 | tail -n +2 | cut -d ' ' -f 1 | sort | uniq | wc -l
-
-
